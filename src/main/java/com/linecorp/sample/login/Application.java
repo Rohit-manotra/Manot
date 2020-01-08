5fc7887cd6f259a5e7def24c3b5e96c04a4d97bd
bestborn14 <59649583+bestborn14@users.noreply.github.com>
2020-01-08 19:41:33 +0530

    updated the code
    used solarLint to provide readibility.

--- src/main/java/com/linecorp/sample/login/Application.java
+++ src/main/java/com/linecorp/sample/login/Application.java
@@ -14,13 +14,14 @@
  * under the License.
  */
 package com.linecorp.sample.login;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * This is sample Application for LINE Platform web login
+ */
 @SpringBootApplication
 public class Application {
-public static void main(final String[] args) throws Exception {
-SpringApplication.run(Application.class, args);
-}
+    public static void main(final String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 }
